fa9bca02a243f0d85cdb66703fd629e5d766dedd
tetsuo <ogunduz@gmail.com>
2014-09-10 20:13:41 +0300

    callback

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ module.exports = parsedeps;
 var acorn = require('acorn');
 var walk = require('acorn/util/walk');
 
-function parsedeps (src) {
+function parsedeps (src, callback) {
   var ast = acorn.parse(src);
   var deps = [];
   walk.simple(ast, {
     CallExpression: function (node) {
       if (node.callee.type === 'Identifier' && 
           node.callee.name === 'require') {
+        if (typeof callback === 'function') fn(node);
         deps.push(node.arguments[0].value);
       }
     }
--- test/index.js
+++ test/index.js
@@ -3,3 +3,8 @@ var assert = require('assert');
 var parsedeps = require('..');
 var deps = parsedeps("require('fs');require('path');");
 assert.deepEqual(deps, ['fs', 'path']);
+function fn0 (node) {
+  node.arguments[0].value += 'x';
+}
+deps = parsedeps("require('fs');require('path');", fn0);
+assert.deepEqual(deps, ['fsx', 'pathx']);
